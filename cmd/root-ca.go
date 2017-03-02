5fc366384291017a58ff36e7967ae98dff903ad7
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-03-02 15:21:54 -0700

    Prevent the root-ca commands from clobbering each other

--- cmd/root-ca.go
+++ cmd/root-ca.go
@@ -6,8 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// root-caCmd represents the root-ca command
-var rootCaCmd = &cobra.Command{
+var rootCAInitCmd = &cobra.Command{
 	Use:   "root-ca",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
@@ -18,31 +17,98 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("root-ca called")
+		fmt.Println("root-ca init called")
+	},
+}
+
+var rootCACheckCmd = &cobra.Command{
+	Use:   "root-ca",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("root-ca check called")
+	},
+}
+
+var rootCARemoveCmd = &cobra.Command{
+	Use:   "root-ca",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("root-ca remove called")
 	},
 }
 
 func init() {
-	initCmd.AddCommand(rootCaCmd)
-	removeCmd.AddCommand(rootCaCmd)
-	checkCmd.AddCommand(rootCaCmd)
-	rootCaCmd.PersistentFlags().StringVar(
+	initCmd.AddCommand(rootCAInitCmd)
+	rootCAInitCmd.PersistentFlags().StringVar(
 		&mount, // defined in root.go
 		"mount",
 		"",
 		"The path in Vault to the intermediate CA pki backend.",
 	)
-	rootCaCmd.PersistentFlags().StringVar(
+	rootCAInitCmd.PersistentFlags().StringVar(
 		&role, // defined in root.go
 		"role",
 		"",
 		"The name of the role to use for operations on the intermediate CA.",
 	)
-	rootCaCmd.PersistentFlags().StringVar(
+	rootCAInitCmd.PersistentFlags().StringVar(
 		&commonName, // defined in root.go
 		"common-name",
 		"",
 		"The common name to use for operations on the intermediate CA.",
 	)
 
+	removeCmd.AddCommand(rootCARemoveCmd)
+	rootCARemoveCmd.PersistentFlags().StringVar(
+		&mount, // defined in root.go
+		"mount",
+		"",
+		"The path in Vault to the intermediate CA pki backend.",
+	)
+	rootCARemoveCmd.PersistentFlags().StringVar(
+		&role, // defined in root.go
+		"role",
+		"",
+		"The name of the role to use for operations on the intermediate CA.",
+	)
+	rootCARemoveCmd.PersistentFlags().StringVar(
+		&commonName, // defined in root.go
+		"common-name",
+		"",
+		"The common name to use for operations on the intermediate CA.",
+	)
+
+	checkCmd.AddCommand(rootCACheckCmd)
+	rootCACheckCmd.PersistentFlags().StringVar(
+		&mount, // defined in root.go
+		"mount",
+		"",
+		"The path in Vault to the intermediate CA pki backend.",
+	)
+	rootCACheckCmd.PersistentFlags().StringVar(
+		&role, // defined in root.go
+		"role",
+		"",
+		"The name of the role to use for operations on the intermediate CA.",
+	)
+	rootCACheckCmd.PersistentFlags().StringVar(
+		&commonName, // defined in root.go
+		"common-name",
+		"",
+		"The common name to use for operations on the intermediate CA.",
+	)
 }
