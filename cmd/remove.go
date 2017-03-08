1bd79b7e57e85910f8b9d21949f2874f76cd52b6
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-03-08 14:43:58 -0700

    Remove commented out code

--- cmd/remove.go
+++ cmd/remove.go
@@ -2,14 +2,10 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-// removeCmd represents the remove command
 var removeCmd = &cobra.Command{
 	Use:   "remove",
 	Short: "Clears out the resources associated with the subcommands from Vault.",
 	Long:  `Clears out the resources associated with the subcommands from Vault.`,
-	// Run: func(cmd *cobra.Command, args []string) {
-	//
-	// },
 }
 
 func init() {
