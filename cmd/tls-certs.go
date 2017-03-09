08e5bf66a9de69a3d12a778be205e0bfb995cf6d
John Wregglesworth <wregglej@iplantcollaborative.org>
2017-03-09 08:45:18 -0700

    Add flags for the 'generate tls' command

--- cmd/tls-certs.go
+++ cmd/tls-certs.go
@@ -7,6 +7,8 @@ type TLSGen struct {
 	mount      string
 	role       string
 	commonName string
+	certPath   string
+	keyPath    string
 	Check      *cobra.Command
 	Generate   *cobra.Command
 	Revoke     *cobra.Command
@@ -35,6 +37,37 @@ func NewTLSGen() *TLSGen {
 	t.Check.Run = t.checkRun
 	t.Generate.Run = t.generateRun
 	t.Revoke.Run = t.revokeRun
+
+	t.Generate.PersistentFlags().StringVar(
+		&t.mount,
+		"mount",
+		defaultIntMount,
+		"The path in Vault to the intermediate CA backend.",
+	)
+	t.Generate.PersistentFlags().StringVar(
+		&t.role,
+		"role",
+		"",
+		"The role to create for generating TLS certs/keys. Should be different for each site.",
+	)
+	t.Generate.PersistentFlags().StringVar(
+		&t.commonName,
+		"common-name",
+		"",
+		"The common name to use when generating the TLS certs/keys.",
+	)
+	t.Generate.PersistentFlags().StringVar(
+		&t.certPath,
+		"cert-path",
+		"",
+		"The file path for the TLS cert. Should be writable.",
+	)
+	t.Generate.PersistentFlags().StringVar(
+		&t.keyPath,
+		"key-path",
+		"",
+		"The file path for the TLS key. Should be writable.",
+	)
 
 	return t
 }
